45b48d48dff8fd97663e13ad9b9514631a93501c
RedTheDragon <TheGoldenCoder@outlook.com>
2015-05-31 08:05:36 -0400

    Update Command_admininfo.java

--- src/me/redthedragon/dragonutils/commands/Command_admininfo.java
+++ src/me/redthedragon/dragonutils/commands/Command_admininfo.java
@@ -19,9 +19,9 @@ public class Command_admininfo extends TFM_Command
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
         TFM_Util.playerMsg(sender_p, "So you wish to apply for admin do you?", ChatColor.BLUE);
-        TFM_Util.playerMsg(sender_p, "Go to our forums at http://freedomroamingmc.boards.net", ChatColor.GREEN);
+        TFM_Util.playerMsg(sender_p, "Go to our forums at http://islandfreedom.boards.net", ChatColor.GREEN);
         TFM_Util.playerMsg(sender_p, "Create a thread in the Super Admin applications forum following the template:", ChatColor.GREEN);
-        TFM_Util.playerMsg(sender_p, "http://freedomroamingmc.boards.net/thread/2/super-admin-application-template", ChatColor.GREEN);
+        TFM_Util.playerMsg(sender_p, "http://islandfreedom.boards.net/thread/2/super-admin-application-template", ChatColor.GREEN);
         TFM_Util.playerMsg(sender_p, "wait for admin responses on your application and an eventual verfdict.", ChatColor.GREEN);
         TFM_Util.playerMsg(sender_p, "Don't ask for admins to look at the application, it will lower your chances of being accepted.", ChatColor.RED);        return true;
     }
