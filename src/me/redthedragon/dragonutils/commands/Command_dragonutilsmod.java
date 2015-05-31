e506f6c07962e84dafff21e8feecf7a416badcd7
RedTheDragon <TheGoldenCoder@outlook.com>
2015-05-31 07:58:27 -0400

    Create Command_dragonutilsmod.java

---
+++ src/me/redthedragon/dragonutils/commands/Command_dragonutilsmod.java
@@ -0,0 +1,35 @@
+/*
+BukkitLib not created by the developers of this plugin
+Plugin AlexUtilsMod is public and open source
+To find bukkit lib go to https://github.com/Pravian/BukkitLib
+(C) RedTheDragon_ 2015 - 2015 This Plugin
+ */
+package me.redthedragon.dragonutils.commands;
+
+import me.redthedragon.dragonutils.DragonUtils;
+import net.pravian.bukkitlib.command.BukkitCommand;
+import net.pravian.bukkitlib.command.CommandPermissions;
+import net.pravian.bukkitlib.command.SourceType;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+/**
+ * AlexUtilsMod Command
+ * @author Alex33856
+ */
+// Using BukkitLib permissions 
+@CommandPermissions(source = SourceType.ANY)
+public class Command_alexutilsmod extends BukkitCommand<DragonUtils> {
+
+    @Override
+    public boolean run(CommandSender sender, Command command, String commandLabel, String[] args) {
+        if (args.length > 0) {
+            return showUsage();
+        }
+        sender.sendMessage(ChatColor.BLUE + "DragonUtils created by RedTheDragon_!");
+        sender.sendMessage(ChatColor.BLUE + "Running Version " + ChatColor.GREEN + this.plugin.getVersion() + "!");
+        return true;
+    }
+    
+}
